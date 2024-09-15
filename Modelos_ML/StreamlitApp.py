d3d3bfad63dc8a34e6db872152f160e97424eed9
CarryARG <lucaas_carranza@hotmail.com>
2024-09-15 16:04:51 -0300

    pruebas

--- Modelos_ML/StreamlitApp.py
+++ Modelos_ML/StreamlitApp.py
@@ -9,7 +9,7 @@ model = joblib.load('./Modelos_ML/random_forest_regressor.joblib')
 df = pd.read_csv('./Data/car_resale_prices_clean.csv')
 
 # Cargar el dataset de costos operacionales
-df_costos = pd.read_csv('./Data/costo_operacional_vehiculos_clean.csv')
+df = pd.read_csv('./Data/costo_operacional_vehiculos_clean.csv', sep=',')
 
 # Calcular umbrales de costos para clasificación
 low_cost_threshold = df['Total_Cost'].quantile(0.33)
