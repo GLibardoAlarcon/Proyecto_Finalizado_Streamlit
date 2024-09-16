394d231bf061eafb14dac3bccbee069062341af4
GLibardoAlarcon <libardoalarcon96@gmail.com>
2024-09-16 18:31:18 -0500

    Cambio de nombre columnas

--- Modelos_ML/Streamlit_app.py
+++ Modelos_ML/Streamlit_app.py
@@ -10,23 +10,23 @@ model = joblib.load('./Modelos_ML/random_forest_regressor.joblib')
 df_costos = pd.read_csv('./Data/car_resale_prices_clean.csv')
 
 # Asegurarnos de que resale_price sea numérico
-df_costos['Resale_Price'] = pd.to_numeric(df_costos['Resale_Price'], errors='coerce')
+df_costos['resale_price'] = pd.to_numeric(df_costos['resale_price'], errors='coerce')
 
 # Verificar si hay valores nulos o incorrectos en resale_price
-df_costos = df_costos.dropna(subset=['Resale_Price'])
+df_costos = df_costos.dropna(subset=['resale_price'])
 
 # Cargar el dataset de costos operacionales
 df = pd.read_csv('./Data/costo_operacional_vehiculos_clean.csv', sep=',')
 
 # Calcular umbrales de costos para clasificación
-low_cost_threshold = df_costos['Resale_Price'].quantile(0.33)
-high_cost_threshold = df_costos['Resale_Price'].quantile(0.66)
+low_cost_threshold = df_costos['resale_price'].quantile(0.33)
+high_cost_threshold = df_costos['resale_price'].quantile(0.66)
 
 # Revisa la función de categorización de vehículos
 def categorize_vehicle(row):
-    if row['Fuel_Type'] in ['Diesel', 'Petrol', 'Petrol/LPG']:
+    if row['fuel_type'] in ['Diesel', 'Petrol', 'Petrol/LPG']:
         return 'Convencional'
-    elif row['Fuel_Type'] in ['Electricity', 'Electric']:
+    elif row['fuel_type'] in ['Electricity', 'Electric']:
         return 'Eléctrico'
     else:
         return 'Híbrido'
@@ -45,16 +45,16 @@ if presupuesto_cliente > 0:
     st.write(f'Autos recomendados dentro de tu presupuesto de ${presupuesto_cliente}:')
     
     # Filtrar los autos dentro del presupuesto
-    autos_recomendados = df_costos[df_costos['Resale_Price'] <= presupuesto_cliente].sort_values(by='Resale_Price', ascending=False).head(5)
+    autos_recomendados = df_costos[df_costos['resale_price'] <= presupuesto_cliente].sort_values(by='resale_price', ascending=False).head(5)
     
     # Si hay autos que cumplen el criterio
     if not autos_recomendados.empty:
-        st.write(autos_recomendados[['Full_Name', 'Registered_Year', 'Fuel_Type', 'Resale_Price', 'Vehicle_Type']].rename(
+        st.write(autos_recomendados[['full_name', 'registered_year', 'fuel_type', 'resale_price', 'Vehicle_Type']].rename(
             columns={
-                'Full_Name': 'Nombre Completo',
-                'Registered_Year': 'Año',
-                'Fuel_Type': 'Tipo de Combustible',
-                'Resale_Price': 'Precio en Dólares',
+                'full_name': 'Nombre Completo',
+                'registered_year': 'Año',
+                'fuel_type': 'Tipo de Combustible',
+                'resale_price': 'Precio en Dólares',
                 'Vehicle_Type': 'Clasificación de Combustible'
             }
         ).round({'Precio en Dólares': 2}))
@@ -63,17 +63,17 @@ if presupuesto_cliente > 0:
 
 # Mostrar detalles del auto seleccionado
 st.write('Detalles del auto seleccionado:')
-autos_list = df_costos['Full_Name'].unique()
+autos_list = df_costos['full_name'].unique()
 selected_auto = st.selectbox('Selecciona un auto:', autos_list)
 
 if selected_auto:
-    auto_data = df_costos[df_costos['Full_Name'] == selected_auto]
-    st.write(auto_data[['Full_Name', 'Registered_Year', 'Fuel_Type', 'Resale_Price', 'Vehicle_Type']].rename(
+    auto_data = df_costos[df_costos['full_name'] == selected_auto]
+    st.write(auto_data[['full_name', 'registered_year', 'fuel_type', 'resale_price', 'Vehicle_Type']].rename(
         columns={
-            'Full_Name': 'Nombre Completo',
-            'Registered_Year': 'Año',
-            'Fuel_Type': 'Tipo de Combustible',
-            'Resale_Price': 'Precio en Dólares',
+            'full_name': 'Nombre Completo',
+            'registered_year': 'Año',
+            'fuel_type': 'Tipo de Combustible',
+            'resale_price': 'Precio en Dólares',
             'Vehicle_Type': 'Clasificación de Combustible'
         }
     ).round({'Precio en Dólares': 2}))
